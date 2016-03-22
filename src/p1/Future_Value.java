ea9e99dd6a02a74d64da9942511e49c73bbd8559
mmendez9 <hikari1995>
2016-03-22 10:48:04 -0400

    Initial commit

---
+++ src/p1/Future_Value.java
@@ -0,0 +1,22 @@
+package p1;
+
+/*
+ * Created by mmendez9 on 3/22/2016.
+ */
+
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Future_Value extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Pane pane = new Pane();
+        pane.setPadding(new Insets(10, 10, 10, 10));
+
+        TextField txAmount = new TextField();
+    }
+}
