e2bb9748311c3543ada933796e71424aea1052dd
mmendez9 <hikari1995>
2016-03-24 09:36:50 -0400

    Info added

--- src/p1/Future_Value.java
+++ src/p1/Future_Value.java
@@ -9,7 +9,6 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.HPos;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
